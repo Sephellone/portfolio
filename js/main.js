b976a919e4ff96fef60d6523bad3cba7b4d806f4
Sephellone <104438793+Sephellone@users.noreply.github.com>
2023-03-27 07:35:25 +0000

    Deploying to gh-pages from @ Sephellone/portfolio@2266a81c5eaba0a745988b57cc0c940ded86f5db 🚀

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-import{initBurger}from"./modules/burger.js";import{initThemeButtons}from"./modules/theme-buttons.js";window.addEventListener("load",(()=>{initThemeButtons(),initBurger()}));
+import{initBurger}from"./modules/burger.js";import{initProjectsSection}from"./modules/projects.js";import{initThemeButtons}from"./modules/theme-buttons.js";window.addEventListener("load",(()=>{initThemeButtons(),initBurger(),initProjectsSection()}));
